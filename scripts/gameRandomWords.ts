40bd8de7863e9b39bb5b94017e338f24c9b15a99
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-04-30 17:50:45 +0530

    HangMan: Add initial array data

---
+++ scripts/gameRandomWords.ts
@@ -0,0 +1,149 @@
+
+const gameWords =  [
+            {
+                "word": "marie curie",
+                "clue": "French-Polish scientist who used to carry radium in her pocket",
+            },
+            {
+                "word": "nasa",
+                "clue": "American space program, abbr.",
+            },
+            {
+                "word": "braille",
+                "clue": "A way for the visually impaired to read",
+            },
+            {
+                "word": "media",
+                "clue": "Marshall McLuhan's main field of study",
+                
+            },
+            {
+                "word": "tele",
+                "clue": "Prefix for vision or phone",
+                
+            },
+            {
+                "word": "computer",
+                "clue": "What once filled an entire room now fits in our pockets",
+                
+            },
+            {
+                "word": "matter",
+                "clue": "Dark ______, substance which occupies most of [9E-D]",
+               
+            },
+            {
+                "word": "neutral",
+                "clue": "State of an atom with an equal number of electrons and protons",
+               
+            },
+            {
+                "word": "sms",
+                "clue": "Phone message, abbr.",
+                
+            },
+            {
+                "word": "smithsonian",
+                "clue": "American's largest museum complex",
+               
+            },
+            {
+                "word": "ada lovelace",
+                "clue": "English mathematician considered to be the first computer programmer",
+              
+            },
+            {
+                "word": "mine kafon",
+                "clue": "Autonomous wind-powered minesweeper",
+               
+            },
+            {
+                "word": "mole",
+                "clue": "The basic unit of measurement for substance",
+               
+            },
+            {
+                "word": "sonar",
+                "clue": "Sound-based navigation method",
+                
+            },
+            {
+                "word": "fiction",
+                "clue": "A genre of literature based on futuristic imagination",
+               
+            },
+            {
+                "word": "mae c jemison",
+                "clue": "First black woman to travel into space",
+               
+            },
+            {
+                "word": "robot",
+                "clue": "Computer programmed machine",
+               
+            },
+            {
+                "word": "email",
+                "clue": "Computer message",
+               
+            },
+            {
+                "word": "url",
+                "clue": "Web browser entry, abbr.",
+               
+            },
+            {
+                "word": "add",
+                "clue": "One of the four basic operations of math",
+               
+            },
+            {
+                "word": "lock",
+                "clue": "What a key opens",
+              
+            },
+            {
+                "word": "airplane",
+                "clue": "The Wrights Brothers' invention",
+               
+            },
+            {
+                "word": "pluto",
+                "clue": "Celestial body orbiting the sun, demoted to a dwarf planet in 2006",
+                
+            },
+            {
+                "word": "turbine",
+                "clue": "Key component in wind power plants",
+               
+            },
+            {
+                "word": "kilowatt",
+                "clue": "Unit of measurement for electrical power",
+               
+            },
+            {
+                "word": "station",
+                "clue": "Terminal for trains and buses - there's even one in [9E-D]",
+                
+            },
+            {
+                "word": "space",
+                "clue": "Where [7A-A], [5K-D] and [11L-D] come from",
+               
+            },
+            {
+                "word": "comet",
+                "clue": "Interstellar traveller with a shiny tail",
+               
+            },
+            {
+                "word": "cern",
+                "clue": "Home to the Large Hadron Collider, abbr.",
+               
+            }
+]
+
+
+
+export default gameWords;
